90d24ae2cd25468cd389dccd0a2c57333983846f
v1c70r-dev <96141283+v1c70r-dev@users.noreply.github.com>
2026-04-11 06:17:52 -0400

    add api path

--- tennis-app-frontend/vite.config.ts
+++ tennis-app-frontend/vite.config.ts
@@ -3,12 +3,20 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_GATEWAY ?? "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
 })
